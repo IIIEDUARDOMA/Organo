6335ac4db572e5f8ae7dccf92364583f2ce3239b
Eduardo Martins <edu.almeida.01@hotmail.com>
2023-06-01 20:25:29 -0300

    Criando componente Times e estilizando

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -5,9 +5,9 @@ const ListaSuspensa = (props) => {
       <div className="lista-suspensa">
          <label>{props.label}</label>
          <select
-            onChange={props.aoAlterado}
+            onChange={(evento) => props.aoAlterado(evento.target.value)}
             required={props.required}
-            value={props.valor}
+            value={props.value}
          >
             {props.itens.map((item) => (
                <option key={item}>{item}</option>
